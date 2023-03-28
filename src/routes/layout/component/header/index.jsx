01c16ed3c107ccbd4687644105d83706006825c8
Mohammad Reza Madankar <m.reza.madankar@gmail.com>
2023-03-27 22:53:30 -0700

    improve hedader style

--- src/routes/layout/component/header/index.jsx
+++ src/routes/layout/component/header/index.jsx
@@ -1,8 +1,173 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Boy from "asset/images/boy.png";
+import Iran from "asset/images/iran.png";
+import Kingdom from "asset/images/kingdom.png";
+import Canada from "asset/images/canada.png";
+import Spain from "asset/images/spain.png";
 
 const Header = () => {
+  const [drpMenu, setDrpMenu] = useState("");
+
+  const changeSetDrpMenu = (drp) => {
+    if (drpMenu === drp) {
+      setDrpMenu("");
+    } else {
+      setDrpMenu(drp);
+    }
+  };
+
   return (
-   <h1>Header</h1>
+    <>
+      <div className="left">
+        <div className="formController">
+          <input type="text" placeholder="search" />
+          <i className="fa fa-search"></i>
+        </div>
+      </div>
+      <div className="right">
+        <div className="drpBox">
+          <button type="button" onClick={() => changeSetDrpMenu("user")}>
+            <i className="fa fa-user"></i>
+          </button>
+          <div className={`drpContent ${drpMenu === "user" ? "show" : "hide"}`}>
+            <ul>
+              <li className="drpHeader">
+                <img src={Boy} alt="Mohammad Reza Madankar" />
+                <span>Mohammad Reza Madankar</span>
+              </li>
+              <li>
+                <i className="fa fa-user"></i>
+                <span>Profile</span>
+              </li>
+              <li>
+                <i className="fa fa-key"></i>
+                <span>Password</span>
+              </li>
+              <li>
+                <i class="fa fa-arrow-right-from-bracket"></i>
+                <span>Sign Out</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="drpBox">
+          <button type="button" onClick={() => changeSetDrpMenu("flag")}>
+            <img src={Iran} alt="User Icon" />
+          </button>
+          <div className={`drpContent ${drpMenu === "flag" ? "show" : "hide"}`}>
+            <ul>
+              <li>
+                <img src={Kingdom} alt="Kingdom Language" />
+                <span>Kingdom</span>
+              </li>
+              <li>
+                <img src={Canada} alt="Canada Language" />
+                <span>Canada</span>
+              </li>
+              <li>
+                <img src={Spain} alt="Spain Language" />
+                <span>Spain</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="drpBox">
+          <button
+            type="button"
+            onClick={() => changeSetDrpMenu("notification")}
+          >
+            <i className="fa fa-bell"></i>
+          </button>
+          <div
+            className={`drpContent ${
+              drpMenu === "notification" ? "show" : "hide"
+            }`}
+          >
+            <ul className="notification">
+              <li>
+                <img src={Boy} alt="Notification Image" />
+                <div>
+                  <h3>title</h3>
+                  <p>
+                    If several languages coalesce the grammar
+                    <br />
+                    <span>
+                      <i className="fa fa-clock"></i>3 min ago
+                    </span>
+                  </p>
+                </div>
+              </li>
+              <li>
+                <img src={Boy} alt="Notification Image" />
+                <div>
+                  <h3>title</h3>
+                  <p>
+                    If several languages coalesce the grammar
+                    <br />
+                    <span>
+                      <i className="fa fa-clock"></i>3 min ago
+                    </span>
+                  </p>
+                </div>
+              </li>
+              <li>
+                <img src={Boy} alt="Notification Image" />
+                <div>
+                  <h3>title</h3>
+                  <p>
+                    If several languages coalesce the grammar
+                    <br />
+                    <span>
+                      <i className="fa fa-clock"></i>3 min ago
+                    </span>
+                  </p>
+                </div>
+              </li>
+              <li>
+                <img src={Boy} alt="Notification Image" />
+                <div>
+                  <h3>title</h3>
+                  <p>
+                    If several languages coalesce the grammar
+                    <br />
+                    <span>
+                      <i className="fa fa-clock"></i>3 min ago
+                    </span>
+                  </p>
+                </div>
+              </li>
+              <li>
+                <img src={Boy} alt="Notification Image" />
+                <div>
+                  <h3>title</h3>
+                  <p>
+                    If several languages coalesce the grammar
+                    <br />
+                    <span>
+                      <i className="fa fa-clock"></i>3 min ago
+                    </span>
+                  </p>
+                </div>
+              </li>
+              <li>
+                <img src={Boy} alt="Notification Image" />
+                <div>
+                  <h3>title</h3>
+                  <p>
+                    If several languages coalesce the grammar
+                    <br />
+                    <span>
+                      <i className="fa fa-clock"></i>3 min ago
+                    </span>
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/routes/layout/component/navigation/index.jsx
+++ src/routes/layout/component/navigation/index.jsx
@@ -5,6 +5,7 @@ const Navigation = () => {
   return (
     <nav className="main-navbar">
       <h1>Quick Question</h1>
+      <hr />
       <NavLink end to="/" id="overViewLink">
         <i className="fa fa-home"></i>
         <span>Overview</span>
@@ -14,15 +15,19 @@ const Navigation = () => {
         className="bbt-calendar"
         state={{ from: "calendar/earnings" }}
       >
+        <i className="fa fa-users"></i>
         <span>User</span>
       </NavLink>
       <NavLink to="/category" className="bbt-simulator">
+        <i className="fa fa-folder-plus"></i>
         <span>Category</span>
       </NavLink>
       <NavLink to="/content" className="bbt-simulator">
+        <i className="fa fa-file-word"></i>
         <span>Blog</span>
       </NavLink>
       <NavLink to="/question/0" className="bbt-scanner">
+        <i class="fa fa-comment"></i>
         <span>Question</span>
       </NavLink>
     </nav>
