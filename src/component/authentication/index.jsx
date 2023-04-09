0e54ef46ba691b37391ab6ab8609dc7e5074686d
Mohammad Reza Madankar <m.reza.madankar@gmail.com>
2023-04-09 10:38:41 -0700

    check user authenticated

--- src/component/authentication/index.jsx
+++ src/component/authentication/index.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 import { useLocation, Navigate } from "react-router-dom";
 import { useUserState } from "utilities/user";
+import axios from "utilities/axios";
 
 export default function Authentication({ children }) {
   const location = useLocation();
-  const { isUserAuthenticated } = useUserState();
+  const { isUserAuthenticated, resetUser, getUser } = useUserState();
   const userLoggedIn = isUserAuthenticated();
+  const [isLoggin, setIsLoggin] = useState(userLoggedIn);
+
+  useEffect(() => {
+    if (isLoggin === true) {
+      axios
+        .get("api/Auth/authenticated")
+        .then(() => {
+          setIsLoggin(true);
+          resetUser();
+        })
+        .catch(() => {
+          toast.error("Your token expired.");
+          setIsLoggin(false);
+        });
+    }
+  }, []);
 
   return (
     <>
-      {!userLoggedIn && (
-        <Navigate to="/login" state={{ from: location }} replace />
-      )}
-      { children }
+      {!isLoggin && <Navigate to="/login" state={{ from: location }} replace />}
+      {children}
     </>
   );
 }
--- src/utilities/axios.js
+++ src/utilities/axios.js
@@ -1,14 +1,19 @@
-import axios from 'axios';
-import config from './config';
+import axios from "axios";
+import { useUserState } from "utilities/user";
+import config from "./config";
 
 const instance = axios.create({
-
-    baseURL: 'http://localhost:5280',
-    timeout: 10000,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept-Language': localStorage.getItem('language') || 'en'
-    }
+  baseURL: "http://localhost:5280",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept-Language": localStorage.getItem("language") || "en",
+    Authorization: `Bearer ${
+      localStorage.getItem("user")
+        ? JSON.parse(localStorage.getItem("user")).token
+        : "notLoggin"
+    }`,
+  },
 });
 
 export default instance;
