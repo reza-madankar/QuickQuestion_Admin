9d3c5e397363c073ddfd67a7421a971d89c95855
Mohammad Reza Madankar <m.reza.madankar@gmail.com>
2023-04-09 11:46:13 -0700

    fix authentication

--- src/component/authentication/index.jsx
+++ src/component/authentication/index.jsx
@@ -1,29 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { toast } from "react-hot-toast";
 import { useLocation, Navigate } from "react-router-dom";
 import { useUserState } from "utilities/user";
-import axios from "utilities/axios";
+
 
 export default function Authentication({ children }) {
   const location = useLocation();
-  const { isUserAuthenticated, resetUser, getUser } = useUserState();
+  const { isUserAuthenticated } = useUserState();
   const userLoggedIn = isUserAuthenticated();
   const [isLoggin, setIsLoggin] = useState(userLoggedIn);
-
-  useEffect(() => {
-    if (isLoggin === true) {
-      axios
-        .get("api/Auth/authenticated")
-        .then(() => {
-          setIsLoggin(true);
-          resetUser();
-        })
-        .catch(() => {
-          toast.error("Your token expired.");
-          setIsLoggin(false);
-        });
-    }
-  }, []);
 
   return (
     <>
